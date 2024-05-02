d8967da635b00e6e54d4ad8956ed5c431540279d
Kirill Smushkin <format3473@gmail.com>
2024-05-02 20:42:09 +0300

    css react fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,66 @@
-const path = require('path'); //для того чтобы превратить относительный путь в абсолютный, мы будем использовать пакет path
-const HTMLWebpackPlugins = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин 
+const path = require("path");
+const HTMLWebpackPlugins = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: path.resolve(__dirname, './src/index.ts'), //точка входа в наше приложение содержит абсолютный путь к index.ts
-    output: {
-        path: path.resolve(__dirname, './dist'), //путь, куда будет собираться наш проект
-        filename: 'main.js', // имя нашего бандла
-    },
-    //Нужно помочь вебпаку научиться работать с jsx- и tsx-файлами, для этого используют ts-loader
-    module: {
-        rules: [
-            {
-                test: /\.[tj]sx?$/, //содержит регулярное выражение, которое содержит информацию, какие файлы должны обрабатываться этим loader'ом
-                use: [
-                    {
-                        loader: 'ts-loader',
-                    },
-                ], // для того чтобы ts-loader корректно отработал, нам нужен tsconfig, его можно создать вручную, а можно создать автоматически
-                /** чтобы проинициализировать его автоматически, можно установить пакет typesctipt глобально или использовать npx, выполнив команду npx tsc --init
+  entry: path.resolve(__dirname, "./src/index.ts"), //точка входа в наше приложение содержит абсолютный путь к index.ts
+  output: {
+    path: path.resolve(__dirname, "./dist"), //путь, куда будет собираться наш проект
+    filename: "main.js", // имя нашего бандла
+  },
+  //Нужно помочь вебпаку научиться работать с jsx- и tsx-файлами, для этого используют ts-loader
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/, //содержит регулярное выражение, которое содержит информацию, какие файлы должны обрабатываться этим loader'ом
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ], // для того чтобы ts-loader корректно отработал, нам нужен tsconfig, его можно создать вручную, а можно создать автоматически
+        /** чтобы проинициализировать его автоматически, можно установить пакет typesctipt глобально или использовать npx, выполнив команду npx tsc --init
                 После создания конфига нужно включить "allowJs": true, чтобы работать не только c typescript, также меняем "jsx": "react", чтобы мы могли работать с react-компонентами, и включаем карту ресурсов "sourceMap": true, пока на этом все вернёмся в этот конфиг позже*/
-                exclude: /node_modules/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                mode: "local",
+                localIdentName: "[name]__[local]__[hash:base64:5]",
+                auto: /\.module\.\w+$/i,
+              },
+              importLoaders: 2, //Значение 2 говорит о том, что некоторые трансформации PostCSS нужно применить до css-loader.
             },
+          },
+          "postcss-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
         ],
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.tsx', '.ts', '.json'], //указываем файлы, с которыми будет работать webpack
-    },
-    plugins: [
-      new HTMLWebpackPlugins({
-         template: path.resolve(__dirname, 'public/index.html')
-      }),
-      new CleanWebpackPlugin(), // использовали плагин
-   ]
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".tsx", ".ts", ".json"],
+  },
+
+  plugins: [
+    new HTMLWebpackPlugins({
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "static/styles/index.css",
+    }),
+  ],
 };
