30fb7082615d6af2a58261fc4852cb865dd59e74
Kirill Smushkin <format3473@gmail.com>
2024-05-03 01:01:07 +0300

    1.2

---
+++ dist/static/scripts/main.114d460278a522f3cd6d.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";var e={};(e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})})(e),console.log(e.default);var o=[2,3,5].map((function(e){return 2*e}));console.log(o)})();
